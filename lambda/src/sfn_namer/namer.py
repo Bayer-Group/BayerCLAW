aa578e3d99c0b18d733bc55b7a41a26941e54fde
jetaba <jack.e.tabaska@monsanto.com>
2022-07-14 13:48:38 -0500

    add sqs queue

--- lambda/src/sfn_namer/namer.py
+++ lambda/src/sfn_namer/namer.py
@@ -5,65 +5,97 @@ import re
 import boto3
 
 
-def make_execution_name0(s3_key: str, version: str, max_len=80) -> str:
-    raw_name = os.path.splitext(os.path.basename(s3_key))[0]
-    # replace illegal chars
-    normalized_name = re.sub(r"[^A-Za-z0-9]+", "_", raw_name)
+# def make_execution_name0(s3_key: str, version: str, max_len=80) -> str:
+#     raw_name = os.path.splitext(os.path.basename(s3_key))[0]
+#     replace illegal chars
+    # normalized_name = re.sub(r"[^A-Za-z0-9]+", "_", raw_name)
     # shorten
-    shortened_name = normalized_name[:max_len - len(version) - 1]
+    # shortened_name = normalized_name[:max_len - len(version) - 1]
     # append version
-    ret = shortened_name + "." + version
+    # ret = shortened_name + "." + version
+    # return ret
+
+
+# def make_execution_name1(s3_key: str, version: str, max_len=80, version_len=8) -> str:
+    # assume the filename extension is something uninformative like ".json"
+    # key_root = os.path.splitext(s3_key)[0]
+    # top level dir is constant for a workflow so discard it
+    # key_root2 = key_root.split("/", 1)[1]
+
+    # in case I ever want to enable step functions logging
+    # normalized_version = version.replace(".", "-")
+    # normalized_name = re.sub(r"[^A-Za-z0-9]+", "-", key_root2)
+
+    # shortened_version = normalized_version[:version_len]
+    # shortened_name = normalized_name[: max_len - 1 - len(shortened_version)].rstrip("-")
+    # ret = shortened_name + "_" + shortened_version
+    # return ret
+
+
+def normalize(key: str) -> str:
+    # assume the filename extension is something uninformative like ".json"
+    root0 = os.path.splitext(key)[0]
+    # top level dir is constant for a workflow so discard it
+    root = re.split(r"/+", root0, maxsplit=1)[-1]
+    ret = re.sub(r"[^A-Za-z0-9]+", "-", root)
     return ret
 
 
-def make_execution_name(s3_key: str, version: str, max_len=80, version_len=8) -> str:
-    # assume the filename extension is something uninformative like ".json"
-    key_root = os.path.splitext(s3_key)[0]
-    # top level dir is constant for a workflow so discard it
-    key_root2 = key_root.split("/", 1)[1]
-
-    # in case I ever want to enable step functions logging
-    normalized_version = version.replace(".", "-")
-    normalized_name = re.sub(r"[^A-Za-z0-9]+", "-", key_root2)
-
-    shortened_version = normalized_version[:version_len]
-    shortened_name = normalized_name[: max_len - 1 - len(shortened_version)].rstrip("-")
-    ret = shortened_name + "_" + shortened_version
+def make_execution_name(s3_key: str, version: str, replay: str) -> str:
+    norm_key = normalize(s3_key)
+    if replay == "":
+        ret = f"{norm_key:.71}_{version:.8}"
+    else:
+        ret = f"{replay:.10}_{norm_key:.60}_{version:.8}"
     return ret
 
 
 def lambda_handler(event: dict, context: object) -> None:
     # event = {
+    #   "Records": [
+    #     {
+    #       "body": <json string>,
+    #       ...and other sqs stuff
+    #     }
+    #   ]
+    # }
+
+    # record = {
     #   "job_file": {
     #     "bucket": "...",
     #     "key": "...",
-    #     "replay": "...",  # empty string if not replay
     #     "version": "..."
     #   },
+    #   "replay": "...",  # empty string if not an archive replay
     #   "index": "main",
     #   "sfn_arn": "..."
     # }
+
     print(str(event))
 
-    state_machine_arn = event.pop("sfn_arn")
-    exec_name = make_execution_name(event["job_file"]["key"], event["job_file"]["version"])
+    for record_str in event["Records"]:
+        record = json.loads(record_str["body"])
+        state_machine_arn = record.pop("sfn_arn")
+        replay = record.pop("replay")
+        exec_name = make_execution_name(record["job_file"]["key"],
+                                        record["job_file"]["version"],
+                                        replay)
 
-    sfn = boto3.client("stepfunctions")
+        sfn = boto3.client("stepfunctions")
 
-    try:
-        while True:
-            response = sfn.start_execution(
-                stateMachineArn=state_machine_arn,
-                name=exec_name,
-                input=json.dumps(event)
-            )
-            # todo: check response["ResponseMetadata"]["HTTPStatusCode"] == 200, crash if not
+        try:
+            while True:
+                response = sfn.start_execution(
+                    stateMachineArn=state_machine_arn,
+                    name=exec_name,
+                    input=json.dumps(record)
+                )
 
-            print(str(response))
+                print(str(response))
 
-            if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-                break
+                if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                    break
 
-    except sfn.exceptions.ExecutionAlreadyExists:
-        # duplicated s3 events are way more likely than bona fide name collisions
-        print(f"duplicate event: {exec_name}")
+        except sfn.exceptions.ExecutionAlreadyExists:
+            # duplicated s3 events are way more likely than bona fide name collisions
+            print(f"duplicate event: {exec_name}")
--- lambda/tests/sfn_namer/test_namer.py
+++ lambda/tests/sfn_namer/test_namer.py
@@ -5,15 +5,50 @@ import boto3
 import moto
 import pytest
 
-from ...src.sfn_namer.namer import make_execution_name, lambda_handler
+from ...src.sfn_namer.namer import normalize, make_execution_name, lambda_handler
 
 
-def test_make_execution_name():
-    key = "one/two/three/four99+five____zireau88*six&^%$#.seven.eight"
-    version = "1234567890ABCDEF1234567890ABCDEF"
-    max_len = 40
-    result = make_execution_name(key, version, max_len=max_len)
-    assert re.match(r"^[A-Za-z0-9_-]{1,40}$", result)
+def test_normalize():
+    key = "DISCARD///two//three/four99+five____zireau88*six&^%$#.seven.DISCARD"
+    expect = "two-three-four99-five-zireau88-six-seven"
+    result = normalize(key)
+    assert result == expect
+
+
+def test_normalize_nothing_to_discard():
+    key = "antidisestablishmentarianism"
+    result = normalize(key)
+    assert result == key
+
+
+@pytest.mark.parametrize("replay", ["", "replay789ABCDEF"])
+def test_make_execution_name_long_key(replay):
+    key = f"dirname/{'a' * 90}"
+    version = "12345678abcdefghijklmnopqrstuvwxyz"
+    result = make_execution_name(key, version, replay)
+
+    assert len(result) == 80
+    assert re.match(r"^[A-Za-z0-9_-]{1,80}$", result)
+
+    assert result.endswith(version[:8])
+    if replay != "":
+        assert result.startswith(replay[:10])
+
+
+@pytest.mark.parametrize("replay", ["", "replay789ABCDEF"])
+def test_make_execution_name_normalize(replay):
+    key = "DISCARD/one!@#$%/TWO)(*&tHrEe<>?/456___789/file-name.DISCARD"
+    exp_norm = "one-TWO-tHrEe-456-789-file-name"
+    version = "12345678abcdefghijklmnopqrstuvwxyz"
+    result = make_execution_name(key, version, replay)
+
+    assert len(result) <= 80
+    assert re.match(r"^[A-Za-z0-9_-]{1,80}$", result)
+
+    assert result.endswith(version[:8])
+    assert exp_norm in result
+    if replay != "":
+        assert result.startswith(replay[:10])
 
 
 @pytest.fixture(scope="function")
@@ -37,15 +72,26 @@ def mock_state_machine():
             yield state_machine["stateMachineArn"]
 
 
-def test_lambda_handler(mock_state_machine):
+@pytest.mark.parametrize("replay, expected_name", [
+    ("", "one-two-three-file_01234567"),
+    ("replay789ABCDEF", "replay789A_one-two-three-file_01234567")
+])
+def test_lambda_handler(mock_state_machine, replay, expected_name):
+    body = {
+        "job_file": {
+            "bucket": "bucket-name",
+            "key": "wf-name/one/two/three/file.txt",
+            "version": "0123456789ABCDEF0123456789abcdef"
+        },
+        "index": "main",
+        "replay": replay,
+        "sfn_arn": mock_state_machine,
+    }
+
     event = {
-      "job_file": {
-        "bucket": "bucket-name",
-        "key": "wf-name/one/two/three/file.txt",
-        "version": "0123456789ABCDEF0123456789abcdef"
-      },
-      "index": "main",
-      "sfn_arn": mock_state_machine,
+        "Records": [
+            {"body": json.dumps(body)}
+        ]
     }
 
     lambda_handler(event, {})
@@ -54,7 +100,7 @@ def test_lambda_handler(mock_state_machine):
     result = sfn.list_executions(stateMachineArn=mock_state_machine)
     execution = result["executions"][0]
     assert execution["stateMachineArn"] == mock_state_machine
-    assert execution["name"] == "one-two-three-file_01234567"
+    assert execution["name"] == expected_name
     assert execution["status"] == "RUNNING"
 
     desc = sfn.describe_execution(executionArn=execution["executionArn"])
@@ -70,15 +116,21 @@ def test_lambda_handler(mock_state_machine):
     assert desc_input == expect
 
 
-def test_namer_duplicate_event(mock_state_machine):
+def test_lambda_handler_duplicate_event(mock_state_machine):
+    body = {
+        "job_file": {
+            "bucket": "bucket-name",
+            "key": "wf-name/four/five/six/file.txt",
+            "version": "abcdef0123456789ABCDEF0123456789"
+        },
+        "index": "main",
+        "replay": "",
+        "sfn_arn": mock_state_machine,
+    }
     event1 = {
-      "job_file": {
-        "bucket": "bucket-name",
-        "key": "wf-name/four/five/six/file.txt",
-        "version": "abcdef0123456789ABCDEF0123456789"
-      },
-      "index": "main",
-      "sfn_arn": mock_state_machine,
+        "Records": [
+            {"body": json.dumps(body)}
+        ]
     }
     event2 = event1.copy()
 
