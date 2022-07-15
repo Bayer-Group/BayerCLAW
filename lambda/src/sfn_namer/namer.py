566c25a94bd6f0902e31b3656ce7aa3553f8ad28
jetaba <jack.e.tabaska@monsanto.com>
2022-07-15 08:23:22 -0500

    fix sqs encryption issue

--- lambda/src/sfn_namer/namer.py
+++ lambda/src/sfn_namer/namer.py
@@ -5,33 +5,6 @@ import re
 import boto3
 
 
-# def make_execution_name0(s3_key: str, version: str, max_len=80) -> str:
-#     raw_name = os.path.splitext(os.path.basename(s3_key))[0]
-#     replace illegal chars
-    # normalized_name = re.sub(r"[^A-Za-z0-9]+", "_", raw_name)
-    # shorten
-    # shortened_name = normalized_name[:max_len - len(version) - 1]
-    # append version
-    # ret = shortened_name + "." + version
-    # return ret
-
-
-# def make_execution_name1(s3_key: str, version: str, max_len=80, version_len=8) -> str:
-    # assume the filename extension is something uninformative like ".json"
-    # key_root = os.path.splitext(s3_key)[0]
-    # top level dir is constant for a workflow so discard it
-    # key_root2 = key_root.split("/", 1)[1]
-
-    # in case I ever want to enable step functions logging
-    # normalized_version = version.replace(".", "-")
-    # normalized_name = re.sub(r"[^A-Za-z0-9]+", "-", key_root2)
-
-    # shortened_version = normalized_version[:version_len]
-    # shortened_name = normalized_name[: max_len - 1 - len(shortened_version)].rstrip("-")
-    # ret = shortened_name + "_" + shortened_version
-    # return ret
-
-
 def normalize(key: str) -> str:
     # assume the filename extension is something uninformative like ".json"
     root0 = os.path.splitext(key)[0]
@@ -75,6 +48,10 @@ def lambda_handler(event: dict, context: object) -> None:
 
     for record_str in event["Records"]:
         record = json.loads(record_str["body"])
+
+        # todo: remove
+        assert "_DIE_DIE_DIE_" not in record["job_file"]["key"]
+
         state_machine_arn = record.pop("sfn_arn")
         replay = record.pop("replay")
         exec_name = make_execution_name(record["job_file"]["key"],
