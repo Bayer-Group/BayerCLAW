b9b0dd0179a856a4c623ab801477f9dded85bde4
jetaba <jack.e.tabaska@monsanto.com>
2022-07-22 07:12:14 -0500

    roll back message test

--- lambda/src/compiler/handler.py
+++ lambda/src/compiler/handler.py
@@ -17,7 +17,6 @@ def lambda_handler(event: dict, context: object) -> dict:
     try:
         ret["fragment"] = compile_template(event["fragment"])
         ret["status"] = "success"
-        ret["errorMessage"] = "this is a message"
 
     except Exception as e:
         # https://stackoverflow.com/questions/55190232/aws-cloudformation-transform-how-do-i-properly-return-an-error-message
