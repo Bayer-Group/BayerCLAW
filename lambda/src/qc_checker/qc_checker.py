96d7e5f59404beb973e08979ff7e6a2190d8f9d6
jetaba <jack.e.tabaska@monsanto.com>
2022-12-14 08:29:04 -0600

    tweak to test layer versioning

--- lambda/src/qc_checker/qc_checker.py
+++ lambda/src/qc_checker/qc_checker.py
@@ -18,7 +18,8 @@ class QCFailed(Exception):
 
 def lambda_handler(event: dict, context: object):
     with custom_lambda_logs(**event["logging"]):
-        logger.info(f"event: {str(event)}")
+        # logger.info(f"event: {str(event)}")
+        logger.info(f"{event=}")
 
         s3_path = f"{event['repo']}/{event['qc_result_file']}"
         bucket, key = s3_path.split("/", 3)[2:]
