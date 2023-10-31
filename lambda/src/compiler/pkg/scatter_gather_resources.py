382d9eff63163afac57f14280069d41bdb2a765c
jetaba <jack.tabaska@bayer.com>
2023-10-31 13:48:46 -0500

    add distributed map code to compiler

--- lambda/src/compiler/pkg/scatter_gather_resources.py
+++ lambda/src/compiler/pkg/scatter_gather_resources.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import re
 from typing import Generator, List
 
 from . import state_machine_resources as sm
@@ -18,59 +19,68 @@ def scatter_step(step: Step, map_step_name: str) -> dict:
             **lambda_logging_block(step.name),
         },
         **lambda_retry(),
-        "ResultPath": "$.items",
+        "ResultPath": "$.scatter",
         "Next": map_step_name
     }
 
     return ret
 
 
-def map_step(sub_branch: dict, gather_step_name: str) -> dict:
-    ret = {
-        "Type": "Map",
-        "ItemsPath": "$.items",
-        "Parameters": {
-            "index.$": "States.Format('{}', $$.Map.Item.Index)",  # stringify the index
-            "job_file.$": "$.job_file",
-            "prev_outputs": {},
-            "repo.$": "$$.Map.Item.Value.repo",
-            "share_id.$": "$.share_id",
-        },
-        "Iterator": sub_branch,
-        "ResultPath": None,
-        "Next": gather_step_name,
-    }
+# def map_step0(sub_branch: dict, gather_step_name: str) -> dict:
+#     ret = {
+#         "Type": "Map",
+#         "ItemsPath": "$.items",
+#         "Parameters": {
+#             "index.$": "States.Format('{}', $$.Map.Item.Index)",  # stringify the index
+#             "job_file.$": "$.job_file",
+#             "prev_outputs": {},
+#             "repo.$": "$$.Map.Item.Value.repo",
+#             "share_id.$": "$.share_id",
+#         },
+#         "Iterator": sub_branch,
+#         "ResultPath": None,
+#         "Next": gather_step_name,
+#     }
+#
+#     return ret
 
+
+def error_tolerance(spec: dict) -> dict:
+    # spec["error_tolerance"]["count"] = 0 by default (set by validator)
+    if "percent" in spec["error_tolerance"]:
+        ret = {"ToleratedFailurePercentage": spec["error_tolerance"]["percent"]}
+    else:
+        ret = {"ToleratedFailureCount": spec["error_tolerance"]["count"]}
     return ret
 
 
-def map_step1(sub_branch: dict, gather_step_name: str) -> dict:
-    step_name = "needStepName"
+def map_step(step: Step, sub_branch: dict, gather_step_name: str) -> dict:
+    label = re.sub(r"\W", "", step.name)
 
     ret = {
-        "Type": "Task",
+        "Type": "Map",
+        "MaxConcurrency": step.spec["max_concurrency"],
+        **error_tolerance(step.spec),
+        "Label": label[:40],
         "ItemReader": {
-            "ReaderConfig": {
-                "InputType": "JSON"
-            },
             "Resource": "arn:aws:states:::s3:getObject",
+            "ReaderConfig": {
+                "InputType": "CSV",
+                "CSVHeaderLocation": "FIRST_ROW",
+            },
             "Parameters": {
-                "Bucket.$": "$.repo.bucket",
-                "Key.$": f"States.Format('{{}}/{step_name}/items.json', $.repo.prefix)",
+                "Bucket.$": "$.scatter.items.bucket",
+                "Key.$": "$.scatter.items.key",
             }
         },
-        # "ItemsPath": {},
         "ItemSelector": {
             "index.$": "States.Format('{}', $$.Map.Item.Index)",  # stringify the index
             "job_file.$": "$.job_file",
             "prev_outputs": {},
-            "repo.$": "$$.Map.Item.Value",
+            "scatter.$": "$$.Map.Item.Value",
+            "repo.$": "$.scatter.repo",
             "share_id.$": "$.share_id"
         },
-        # ItemBatcher?
-        # MaxConcurrency*?
-        # ToleratedFailure*?
-        "Label": step_name,
         "ItemProcessor": {
             "ProcessorConfig": {
                 "Mode": "DISTRIBUTED",
@@ -78,18 +88,39 @@ def map_step1(sub_branch: dict, gather_step_name: str) -> dict:
             },
             **sub_branch
         },
-        "ResultWriter": {
-            "Resource": "arn:aws:states:::s3:putObject",
-            "Parameters": {
-                "Bucket.$": "$.repo.bucket",
-                "Prefix.$": "$.repo.prefix",
-            }
-        },
+        # "ResultWriter": {
+        #     "Resource": "arn:aws:states:::s3:putObject",
+        #     "Parameters": {
+        #         "Bucket.$": "$.repo.bucket",  # ergh
+        #         "Prefix.$": "$.repo.prefix",  # double ergh
+        #     }
+        # },
         "ResultPath": None,
         "Next": gather_step_name,
     }
 
     return ret
+
+
+def scatter_init_step(parent_step_name: str) ->  dict:
+    step_name = f"{parent_step_name}.initialize"
+    ret = {
+        step_name: {
+            "Type": "Task",
+            "Resource": os.environ["SCATTER_INIT_LAMBDA_ARN"],
+            "Parameters": {
+                "index.$": "$.index",
+                "repo.$": "$.repo",
+                "scatter.$": "$.scatter",
+                **lambda_logging_block(step_name)
+            },
+            **lambda_retry(max_attempts=10),
+            "ResultPath": "$.repo",
+            "_stet": True,
+        },
+    }
+    return ret
+
 
 def gather_step(step: Step) -> dict:
     ret = {
@@ -120,7 +151,8 @@ def handle_scatter_gather(step: Step,
     if map_depth > 0:
         raise RuntimeError("Nested Scatter steps are not supported")
 
-    sub_branch = yield from sm.make_branch(step.spec["steps"], options, depth=map_depth + 1)
+    sub_branch = yield from sm.make_branch([scatter_init_step(step.name)] + step.spec["steps"],
+                                           options, depth=map_depth + 1)
 
     scatter_step_name = step.name
     map_step_name = f"{step.name}.map"
@@ -128,7 +160,7 @@ def handle_scatter_gather(step: Step,
 
     ret = [
         State(scatter_step_name, scatter_step(step, map_step_name)),
-        State(map_step_name, map_step(sub_branch, gather_step_name)),
+        State(map_step_name, map_step(step, sub_branch, gather_step_name)),
         State(gather_step_name, gather_step(step))
     ]
 
